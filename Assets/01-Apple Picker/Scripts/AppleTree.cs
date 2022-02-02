6d67cf01488e97f3e97ade58fec23ad2abdc741c
danielliu122 <danielliu122@yahoo.com>
2022-02-02 15:00:13 -0500

    Tree moving
    Tree moving

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -11,7 +11,7 @@ public class AppleTree : MonoBehaviour
     // Distance where AppleTree turns around
     public float leftAndRightEdge = 10f;
     // Chance that the AppleTree will change directions
-    public float chanceToChangeDirections = 0.1f;
+    public float chanceToChangeDirections = 0.05f;
     // Rate at which Apples will be instantiated
     public float secondsBetweenAppleDrops = 1f;
     void Start () {
@@ -19,6 +19,25 @@ public class AppleTree : MonoBehaviour
     }
     void Update () {
         // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        // pos.x += 1.0f * 0.04f;
+        // pos.x += 0.04f;
+        transform.position = pos;
         // Changing Direction
+        if ( pos.x < -leftAndRightEdge ) {
+            speed = Mathf.Abs(speed); // Move right
+        } 
+        else if ( pos.x > leftAndRightEdge ) {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+
     }
+    void FixedUpdate() {
+    // Changing Direction Randomly
+    if ( Random.value < chanceToChangeDirections ) {
+        speed *= -1; // Change direction
+        }
+    }
+
 }
