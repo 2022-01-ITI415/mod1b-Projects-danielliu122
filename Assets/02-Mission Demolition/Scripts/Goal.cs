71f58e1efd27f6dc05af9959f4373760e69de6c3
Daniel Liu <dtl61@scarletmail.rutgers.edu>
2022-02-10 18:44:32 -0500

    trail and goal hit
    trail and goal hit

--- Assets/02-Mission Demolition/Scripts/Goal.cs
+++ Assets/02-Mission Demolition/Scripts/Goal.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-	static public bool 	goalMet = false;
-
-	void OnTriggerEnter(Collider other) {
-		// when the trigger is hit by something
-		// check to see if it's a Projectile 
-		if (other.gameObject.tag == "Projectile") {
-			// if so, set goalMet = true
+	// A static field accessible by code anywhere
+	static public bool goalMet = false;
+	void OnTriggerEnter( Collider other ) {
+		// When the trigger is hit by something
+		// Check to see if it's a Projectile
+		if ( other.gameObject.tag == "Projectile" ) {
+			// If so, set goalMet to true
 			Goal.goalMet = true;
-
-			// also set the alpha of the color of higher opacity
-			Material mat = GetComponent<Renderer>().material;
-			Color c = mat.color;
+			// Also set the alpha of the color to higher opacity
+			Color c = GetComponent<Renderer>().material.color;
 			c.a = 1;
-			mat.color = c;
+			GetComponent<Renderer>().material.color = c;
+			}
 		}
-	}
+	
     // Start is called before the first frame update
     void Start()
     {
--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static public Slingshot S;
+
     // fields set in the Unity Inspector pane
     public GameObject prefabProjectile;
     public float velocityMult = 4f;
@@ -57,6 +59,9 @@ public class Slingshot : MonoBehaviour
         }
     }
     void Awake() {
+        // Set the Slingshot singleton S
+        S = this;
+
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive( false );
